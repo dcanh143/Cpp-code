4f7e3559c0791b34fad9d082f9a4ea55a0c6b1ba
ducanhub00 <ducanhkhtn@gmail.com>
2021-03-31 09:38:29 +0700

    tro choi tim tu

---
+++ TroChoiTimTu/app.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> m >> n;
+    char a[m][n];
+    for(int i = 0; i < m; ++i) {
+        for(int j = 0; j < n; ++j) {
+            cin >> a[i][j];
+        }
+    }
+    
+}
